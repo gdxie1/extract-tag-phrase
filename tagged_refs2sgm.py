2805fc10807a368f8291f083c77225f1689f3806
guodong from office <guodong.xie@adatpcentre.ie>
2019-08-06 15:12:40 +0100

    add porcessing of input args

--- tagged_refs2sgm.py
+++ tagged_refs2sgm.py
@@ -35,22 +35,49 @@ def load_alignment(file_name):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument("--ori_sgm", type=str)
-    parser.add_argument("--tagged_refs", type=str, nargs="+",
-                        help="tagged plain references")
 
+    parser.add_argument("--ori_sgm", type=str)
     parser.add_argument("--output", type=str)
 
+
+
+    # fe_phrases = get_giza_file_content(args.fe_file)
+    # ef_phrases = get_giza_file_content(args.ef_file)
+
+    parser.add_argument("--tagged_refs", type=str, nargs="+",
+                        help="tagged plain references")
+    parser.add_argument("--line_id", type=str, nargs="+",
+                        help="tagged refs's line id in sgm file")
+
     args = parser.parse_args()
-    
-    fe_phrases = get_giza_file_content(args.fe_file)
-    ef_phrases = get_giza_file_content(args.ef_file)
-
-
     senid = 0
 
-    file_f = open(args.f_output, mode='w', encoding="utf-8")
+
+    if len(args.tagged_refs) != len(args.line_id):
+        print("refs file number must equal line id file number")
+        exit(-1)
+
+
+    # read sgm file
+
+    file_sgm = open(args.ori_sgm, mode='rt', encoding="utf-8")
+    file_sgm_list = []
+    for line in file_sgm:
+        file_sgm_list.append(line)
+    for ref, line_id in zip(args.tagged_refs, args.line_id):
+        ref_file = open(ref, mode='rt', encoding='utf-8')
+        line_id_file = open(line_id,  mode='rt', encoding='utf-8')
+        for l_id, line_text in zip(line_id_file, ref_file):
+            ori_text = file_sgm_list[l_id]
+            new_text = re.sub(r'<seg id="[0-9]+"> (.*)</seg>'
+                r"(?i)^.*interfaceOpDataFile.*$",
+           "interfaceOpDataFile %s" % fileIn,
+           line
+       )
+
+        ref_file.close()
+        line_id_file.close()
+
     file_e = open(args.e_output, mode='w', encoding="utf-8")
     for fe_phrase, ef_phrase in zip(fe_phrases, ef_phrases):
         print(senid)
@@ -58,7 +85,7 @@ if __name__ == '__main__':
         # stopline = 1899
         # if senid != stopline:
         #     continue
-            # senid = stopline
+        # senid = stopline
         fe_alignment, ef_alignment = get_alignments(fe_phrase, ef_phrase)
         # fe_phrase[0] 是 e 句子
         f_sen = ' '.join(ef_phrase[0])
@@ -69,3 +96,4 @@ if __name__ == '__main__':
         file_e.write('\n')
     file_f.close()
     file_e.close()
+
