24caa0714600c8d3ce17c965f4d9a1b12fe775d4
guodong from office <guodong.xie@adatpcentre.ie>
2019-08-13 12:04:41 +0100

    to remove tags works

--- remove_tag.py
+++ remove_tag.py
@@ -14,13 +14,16 @@ if __name__ == '__main__':
     parser.add_argument("--output", type=str, required=True, )
 
     args = parser.parse_args()
-
+    #pattern = re.compile(r'(<[a-zA-Z]+) (\w+) ( > )')
     # read input file
     file_input = open(args.input, mode='rt', encoding="utf-8")
     list_result = []
     for line in file_input:
-        search_result = re.search(r'(<[a-zA-Z]+) (.*) (> )', line)
-        new_line = re.sub(r'(<[a-zA-Z]+) (.*) (> )', r'\2', line)
+        # print(line[:-1])
+        # it = re.finditer(r'(<[a-zA-Z]+ )([^>]+)( >)', line)
+        # for match in it:
+        #     print(match.group())
+        new_line = re.sub(r'(<[a-zA-Z]+ )([^>]+)( >)', r'\2', line)
         print(new_line)
         list_result.append(new_line)
     file_e = open(args.output, mode='wt', encoding="utf-8")
