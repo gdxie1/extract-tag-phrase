d2c9da49f354416049e9ded1a53164b041929f52
Gd Xie <gdxie@Gds-iMac.local>
2019-08-25 13:32:31 +0100

    update to python3

--- parser_corpus_save.py
+++ parser_corpus_save.py
@@ -1,64 +1,9 @@
 # coding=utf-8
-# chcp 65001
-# import csv
-import pickle
-import random
 import codecs
 import argparse
-from collections import Counter
-from phrase_extraction import phrase_extraction
-from alignment import get_alignments, do_alignment
 from nltk.parse import CoreNLPParser
 from nltk.tree import Tree
 
-from nltk.tag.stanford import StanfordPOSTagger
-def get_giza_file_content(file_name):
-    file_content = get_data_from_file(file_name)
-    return [(file_content[i + 1].split(), file_content[i + 2])
-            for i in range(0, len(file_content) - 1, 3)]
-
-
-def get_data_from_file(file_name):
-    with codecs.open(file_name, encoding="utf-8") as file_:
-        content = [line.lower().strip() for line in file_]
-    # avoid to be segmented with the Record Seperator
-    # content = []
-    # with codecs.open(file_name, encoding="utf-8") as file_:
-    #     for i, line in enumerate(file_):
-    #         new_line = line.lower().strip()
-    #         if i % 3 == 0 and new_line[0] == u'#': #every three lines there will be the # line
-    #             content.append(new_line)
-    #         else:
-    #             content[i - 1] += new_line
-    return content
-
-
-def load_sentences(file_name):
-    file_content = get_data_from_file(file_name)
-    return [sentence.split() for sentence in file_content]
-
-
-def load_alignment(file_name):
-    file_content = get_data_from_file(file_name)
-    alignment = []
-    for content_item in file_content:
-        single_align = content_item.split()
-        alignment.append([(pairs.split("-")[0], pairs.split("-")[1]) \
-                          for pairs in single_align])
-
-    return alignment
-
-def traverse(t, ph_dict, ph_tag):
-    try:
-        tag = t.label()
-    except AttributeError:
-        return
-    if tag in ph_tag:
-        ph_dict[tag].append(t)  # keep the tree structure
-    for subtree in t:
-        if type(subtree) == Tree:
-            traverse(subtree, ph_dict, ph_tag)
- 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # parser.add_argument("f_file", type=str)
@@ -75,12 +20,12 @@ if __name__ == '__main__':
     exception_sen = []
     tree_list = []
     p_phrase_trees = None
-    f_input = codecs.open(args.input, 'rt', 'utf-8')
+    f_input = open(args.input, mode='rt', encoding='utf-8')
 
-    f_output = codecs.open(args.output, 'wt', 'utf-8')
+    f_output = codecs.open(args.output, mode='wt', encoding='utf-8')
     # f_output = open(args.output, 'wt')
     for senid, line in enumerate(f_input):
-        if senid > 10:
+        if senid > 100:
             break
         try:
             p_parse_trees = list(parser.parse(parser.tokenize(line)))
@@ -104,7 +49,7 @@ if __name__ == '__main__':
     #     pickle.dump(tree_list, f)
 
     print('exception sen:%r' % exception_sen)
-    with codecs.open('tagged_info.txt', mode='w', encoding="utf-8") as f:
+    with open('tagged_info.txt', mode='w', encoding="utf-8") as f:
         f.write('exception sen:%r\n' % exception_sen)
 
 
